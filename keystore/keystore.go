72eaea5aedaecf58b567db6f75b5d05c46709aa9
max <maximogarciamtnez@gmail.com>
2021-08-22 09:18:40 +0200

    renamed keystore

---
+++ keystore/keystore.go
@@ -0,0 +1,43 @@
+package keystore
+
+import (
+	"encoding/hex"
+	"io/ioutil"
+	"os"
+
+	"github.com/ethereum/go-ethereum/accounts"
+	"github.com/ethereum/go-ethereum/accounts/keystore"
+	"github.com/ethereum/go-ethereum/crypto"
+)
+
+// Creates a new keystore and saves it in the path with the given password
+func CreateKs(folder string, password string) (accounts.Account, error) {
+	if _, err := os.Stat(folder); err == nil {
+		return accounts.Account{}, err
+	}
+	ks := keystore.NewKeyStore(folder, keystore.StandardScryptN, keystore.StandardScryptP)
+	account, err := ks.NewAccount(password)
+	if err != nil {
+		return accounts.Account{}, err
+	}
+
+	return account, nil
+}
+
+// getKey get a key from KeyStore
+func ImportKs(file string, password string) (string, string, error) {
+	data, err := ioutil.ReadFile(file)
+	if err != nil {
+		return "", "", err
+	}
+
+	key, err := keystore.DecryptKey(data, password)
+	if err != nil {
+		return "", "", err
+	}
+
+	privKey := hex.EncodeToString(crypto.FromECDSA(key.PrivateKey))
+	pubKey := hex.EncodeToString(crypto.FromECDSAPub(&key.PrivateKey.PublicKey))[2:]
+
+	return privKey, pubKey, nil
+}
---
+++ keystore/keystore_test.go
@@ -0,0 +1,111 @@
+package keystore
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/onmax/go-alastria/crypto"
+	"github.com/onmax/go-alastria/tokens"
+)
+
+var tempFolder string = "test-data__temp"
+
+func TestCreateKs(t *testing.T) {
+	type args struct {
+		folder   string
+		password string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "CreateKeyStore",
+			args: args{
+				folder:   tempFolder,
+				password: "test",
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ks, err := CreateKs(tt.args.folder, tt.args.password)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("CreateKs() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if len(ks.Address.Hex()) != 42 {
+				t.Errorf("CreateKs() = %v, want something with 42 chars", ks.Address.Hex())
+			}
+
+			// remove the folder
+			os.RemoveAll(tt.args.folder)
+		})
+	}
+}
+
+func TestImportKs(t *testing.T) {
+
+	t.Run("Creates and imports the keystore", func(t *testing.T) {
+		_, err := CreateKs("test-data", "password")
+		if err != nil {
+			t.Errorf("CreateKs() error = %v", err)
+			return
+		}
+
+		files, err := ioutil.ReadDir("./test-data")
+		if err != nil {
+			t.Errorf("ImportKs() error = %v", err)
+			return
+		}
+
+		_, pubKey, err := ImportKs("./test-data/"+files[0].Name(), "password")
+		if err != nil {
+			t.Errorf("ImportKs() error = %v", err)
+			return
+		}
+		if len(pubKey) != 128 {
+			t.Errorf("ImportKs() = %v, want something with 128 chars", pubKey)
+		}
+
+		os.RemoveAll("./test-data")
+	})
+
+	t.Run("Signs and verifies a JWT with same keystore", func(t *testing.T) {
+		privKey, pubKey, err := ImportKs("./test-keystore/keystore1.json", "test")
+		if err != nil {
+			t.Errorf("Sign() error = %v", err)
+			return
+		}
+		at := tokens.AT{
+			Header: &tokens.Header{
+				Algorithm:    "ES256K",
+				Type:         "JWT",
+				KeyID:        "key-id",
+				JSONWebToken: "json-web-token",
+			},
+			Payload: &tokens.ATPayload{
+				Issuer:            "issuer",
+				AlastriaNetworkId: "alastria-network-id",
+				CallbackURL:       "callback-url",
+				GatewayURL:        "gateway-url",
+				Types:             []string{"AlastriaToken"},
+			},
+		}
+
+		signed, err := crypto.Sign(at, privKey)
+		if err != nil {
+			t.Errorf("Sign() error = %v", err)
+			return
+		}
+
+		err = crypto.Verify(signed, pubKey)
+		if err != nil {
+			t.Errorf("Verify() error = %v", err)
+			return
+		}
+	})
+}
