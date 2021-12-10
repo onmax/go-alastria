86ba4530465e6da138dd77c98074ffa2afd7092b
Max <onmax@users.noreply.github.com>
2021-12-10 22:53:32 +0000

    fixed import

--- blockchain/tx/tx.go
+++ blockchain/tx/tx.go
@@ -5,7 +5,7 @@ import (
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/onmax/go-alastria/tx/txsender"
+	"github.com/onmax/go-alastria/blockchain/txsender"
 	alaTypes "github.com/onmax/go-alastria/types"
 )
 
