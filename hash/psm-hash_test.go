3d7f1205863cf82cf4dd453f1cf120cecefeeda3
Max <onmax@users.noreply.github.com>
2021-12-11 11:45:08 +0000

    updated test to use did.Did struct

--- hash/psm-hash_test.go
+++ hash/psm-hash_test.go
@@ -3,12 +3,16 @@ package hash
 import (
 	"reflect"
 	"testing"
+
+	"github.com/onmax/go-alastria/did"
+	alaTypes "github.com/onmax/go-alastria/types"
 )
 
 func TestPsmHash(t *testing.T) {
+	d, _ := did.NewDidFromString("did:ala:quor:redT:80d0ff71e3f212cf383e57446df878e348f825db")
 	type args struct {
 		signedJwt string
-		did       string
+		did       *alaTypes.Did
 	}
 	tests := []struct {
 		name  string
@@ -20,7 +24,7 @@ func TestPsmHash(t *testing.T) {
 			name: "test ok",
 			args: args{
 				signedJwt: "eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NksiLCJraWQiOiJkaWQ6YWxhOnF1b3I6cmVkdDoxMmVlYUNDQTllRWJCNzhlQjk3ZDdjYWM2YiNrZXlzLTEifQ.eyJqdGkiOiJodHRwczovL3d3dy5lbXByZXNhLmNvbS9hbGFzdHJpYS9jcmVkZW50aWFscy8zNzM0IiwiaXNzIjoiZGlkOmFsYTpxdW9yOnJlZFQ6NGI4MzQzMjAzNWQxNDc3ZDNhOTliMjNiYWE4MmJkOWRiNjFhMmQ2OCIsInN1YiI6ImRpZDphbGE6cXVvcjpyZWR0OjB4MTJlZWFDQ0E5ZUViQjc4ZUI5N2Q3Y2FjNmIiLCJpYXQiOjE2MzkwOTAyMzUsImV4cCI6MTU2Mzc4MzM5MiwibmJmIjoxNTYzNzgyNzkyLCJ2YyI6eyJAY29udGV4dCI6WyJodHRwczovL3d3dy53My5vcmcvMjAxOC9jcmVkZW50aWFscy92MSIsImh0dHBzOi8vYWxhc3RyaWEuZ2l0aHViLmlvL2lkZW50aXR5L2NyZWRlbnRpYWxzL3YxIiwiaHR0cHM6Ly93M2lkLm9yZy9kaWQvdjEiLCJKV1QiXSwidHlwZSI6WyJWZXJpZmlhYmxlQ3JlZGVudGlhbCIsIkFsYXN0cmlhVmVyaWZpYWJsZUNyZWRlbnRpYWwiXSwiY3JlZGVudGlhbFN1YmplY3QiOnsiU3R1ZGVudElEIjoiMTEyMzU4MTMiLCJsZXZlbE9mQXNzdXJhbmNlIjoxfX19.j4BhQTs1fi1DeFLjI9JWCQIo3Qkk372cq_bZHCo5fTIafqGXcHETnJG6xeZ3F35CeO4sZkSFlpaccTKfdH6kew",
-				did:       "did:ala:quor:redT:80d0ff71e3f212cf383e57446df878e348f825db",
+				did:       d,
 			},
 			want:  "0xbc8f70b6722d1bff3129f17c9b72b27a136475f5bae9ac47b1d36301887b5dfb",
 			want1: [32]byte{48, 120, 98, 99, 56, 102, 55, 48, 98, 54, 55, 50, 50, 100, 49, 98, 102, 102, 51, 49, 50, 57, 102, 49, 55, 99, 57, 98, 55, 50, 98, 50},
